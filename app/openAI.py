11c638be04ea7ebe75aa9d3ead840064ced31a5d
Alexcmad <mcintoshalexander80@gmail.com>
2023-09-19 18:04:36 -0500

    added the wiki search

--- app/openAI.py
+++ app/openAI.py
@@ -4,7 +4,6 @@ All OpenAI based code goes here
 import openai
 from dotenv import load_dotenv
 import os
-from pprint import pprint
 
 load_dotenv()
 api_key = os.getenv("GPT_SECRET")
@@ -28,6 +27,3 @@ def generate_keywords(search_term: str) -> list:
     str_terms: str = completion['choices'][0]['message']['content']
     list_terms: list = str_terms.split(', ')
     return list_terms
-
-
-pprint(generate_keywords("Sociology"))
---
+++ app/wiki.py
@@ -0,0 +1,27 @@
+import wikipedia
+from pprint import pprint
+import json
+
+
+def wiki_search(search_term: str, default: int = 0) -> dict:
+    """
+    :param default: (optional) Default index of option to use in case of Disambiguation Error
+    :param search_term: The term to be searched on wikipedia
+    :return: A dictionary containing information on the term
+    """
+    search_term += '.'
+    try:
+        search_result = wikipedia.page(search_term)
+    except wikipedia.exceptions.DisambiguationError as e:
+        search_term = e.options[default]
+        search_result = wikipedia.page(search_term)
+
+    search_result_dict = {
+        "title": search_result.title,
+        "url": search_result.url,
+        "summary": search_result.summary
+    }
+
+    return search_result_dict
+
+pprint(wiki_search("Dog"))
