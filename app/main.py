100d5942e3cb3743d0a78da083ec12d98841248b
Alexcmad <mcintoshalexander80@gmail.com>
2023-09-20 12:46:17 -0500

    Wrote API

--- app/main.py
+++ app/main.py
@@ -3,11 +3,25 @@ The main driver code of the app (solution) goes here
 FASTAPI stuff
 """
 from fastapi import FastAPI
-from .routers import aiRoutes
+from fastapi.staticfiles import StaticFiles
+from . import wiki, openAI, file_reader
 
 app = FastAPI()
+app.mount("/../static", StaticFiles(directory="static"))
 
 
 @app.get('/')
-def root():
+def root() -> dict:
     return {"status": "working"}
+
+
+@app.get('/wiki/{term}')
+def wiki_search(term: str) -> dict:
+    wiki_data: dict = wiki.wiki_search(term)
+    return wiki_data
+
+
+@app.get('/generate/terms/{term}')
+def generate_terms(term: str) -> dict:
+    generated_terms: dict = openAI.generate_keywords(term)
+    return generated_terms
--- app/openAI.py
+++ app/openAI.py
@@ -11,20 +11,21 @@ api_key = os.getenv("GPT_SECRET")
 openai.api_key = api_key
 
 
-def generate_keywords(search_term: str) -> list:
+def generate_keywords(search_term: str) -> dict:
     """
     :param search_term: The term to be searched
     :return: A list of keywords relevant to the search term
     """
-    with open("../static/keywords_context", "r") as file:
+    with open("static/keywords_context", "r") as file:
         context: str = file.read()
     generation_context: list[dict] = [{"role": "system", "content": context},
                                       {"role": "user", "content": search_term}]
     completion: dict = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=generation_context,
-        temperature=0
+        temperature=0.2
     )
     str_terms: str = completion['choices'][0]['message']['content']
     list_terms: list = str_terms.split(', ')
-    return list_terms
+    return {"term": search_term,
+            "keywords": list_terms}
